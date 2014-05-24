2e03aa21fbf9140ee6d9bddaebf38baacd396d8c
John Curley <johncurley@zoho.com>
2014-05-25 09:11:50 +1200

    add

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns(
@@ -11,4 +12,4 @@ urlpatterns = patterns(
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('hackakl.urls', namespace="hackakl")),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
